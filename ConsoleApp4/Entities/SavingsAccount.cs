2db749e792c6836791182846952006787869197f
hiagomenezes <hiago@itecnologia.com.br>
2019-08-06 17:05:42 -0300

    teste 2

---
+++ ConsoleApp4/Entities/SavingsAccount.cs
@@ -0,0 +1,21 @@
+﻿
+namespace ConsoleApp4.Entities
+{
+    class SavingsAccount : Account
+    {
+        public double  InterestRate{ get; set; }    
+
+        public SavingsAccount()
+        {
+        }
+        public SavingsAccount (int number, string holder, double balance, double interestRate) : base(number, holder, balance)
+        {
+            InterestRate = interestRate;
+        }
+
+        public void UpDateBalance()
+        {
+            Balance += Balance * InterestRate; 
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ConsoleApp4.Entities;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account acc = new Account(1001, "hiago", 0.0);
+            BusinessAccount bacc = new BusinessAccount(1002, "alex", 0.0, 500.0);
+        }
+    }
+}
